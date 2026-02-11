35cd4df96885a1c39ea2cb9ecbd34eaa826c7365
Abishek <83390098+Abishek110@users.noreply.github.com>
2026-02-11 15:17:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -163,6 +163,9 @@ app.post("/analyze", async (req, res) => {
 // =========================
 // SERVER START
 // =========================
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
